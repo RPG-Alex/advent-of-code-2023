0e7d22c282c0b86579ae2801a5397f7734c6cd3b
Alex Blood <mralexblood@outlook.com>
2023-12-06 16:33:29 +0800

    Day 3 part 1 solved, but not optimized, high complexity

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -33,88 +33,92 @@ Of course, the actual engine schematic is much larger. What is the sum of all of
 */
 
 use std::fs::read_to_string;
-use std::collections::HashMap;
 
 fn main() {
     //get input from file
     // Stringify input
-    let mut content = read_to_string("inputs/day3.txt").unwrap();
+    let content = read_to_string("inputs/day3.txt").unwrap();
 
     // Test case:
-    //content = String::from("467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..");
+    //let content = String::from("467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..");
 
-    // Put lines into vector
+    // Get the line length for first line
     let lines: Vec<&str> = content.lines().collect();
+    let line_length = lines.get(0).map_or(0, |line| 
+        line.len()) as i32;
+    // After we have total line length, filter out lines. no longer needed    
+    let content = content.replace('\n', "");
 
-    //get all non period non number characters from file
-    let mut characters = Vec::new();
-    for ch in content.chars() {
-        if !ch.is_digit(10) && ch != '.' && !characters.contains(&ch) {
-            characters.push(ch);
+    // Turn String into a vector of index and char
+    let characters: Vec<_> = content.chars().enumerate().collect();
+    // Take that string and keep only numbers, retaining their location
+    let all_nums_as_chars: Vec<(usize, char)> = characters.clone().into_iter().filter(|&(_,c)|
+        c.is_numeric()
+    ).collect();
+
+
+
+    //Group neighboring numbers and retain their starting and ending locations in the characters vec
+    let mut all_possible_numbers: Vec<(String, usize, usize)> = Vec::new();
+    
+    // Used for iterating and grouping
+    let mut current = String::new();
+    let mut start_index = 0;
+
+    for (i, (index, c)) in all_nums_as_chars.iter().enumerate() {
+        if i == 0 || *index == all_nums_as_chars[i -1].0 + 1{
+            current.push(*c);
+        } else {
+            all_possible_numbers.push((current.clone(), start_index, all_nums_as_chars[i-1].0));
+
+            current = c.to_string();
+            start_index = *index;
         }
     }
+    // Push last sequence
+    if !current.is_empty() {
+        all_possible_numbers.push((current, start_index, all_nums_as_chars.last().unwrap().0));
+    }
 
-    // Running Total
+
+    //Check every possible number for validity and add to total if valid
     let mut total = 0;
-
-    //Iterate the vector using line counts
-    for (i, line) in lines.iter().enumerate() {
-        // set default value for edge cases
-        let top: &str = if i > 0 { lines[i - 1] } else { ".........." };
-        let bottom: &str = if i < lines.len() - 1 { lines[i + 1] } else { ".........." };
-
-        let mut num: HashMap<usize, char> = HashMap::new();
-        let line_chars: Vec<char> = line.chars().into_iter().collect();
-        for (j, c) in line_chars.iter().enumerate() {
-            if c.is_numeric() {
-                num.insert(j, *c);
-            } else {
-                check_number(&mut num, &characters, &line_chars, top, bottom, &mut total, line);
-            }
+    for (s, start, end) in all_possible_numbers {
+        if check_surroundings(start, end, line_length, &content) {
+            println!("({}, {}, {})", s, start, end);
+            total += s.parse::<i32>().unwrap();
         }
-
     }
-    println!("{}", total);
+
+    println!("The grand total is: {}", total);
+    
 }
 
 
-fn check_number(num: &mut HashMap<usize, char>, characters: &[char], line_chars: &[char], top: &str, bottom: &str, total: &mut i32, line: &&str) {
-    if !num.is_empty() {
-        //Check if our number is valid
-        let mut sorted_keys: Vec<usize> = num.keys().cloned().collect();
-        sorted_keys.sort();
-        let mut valid = false;
+// Should return true if its a valid number and false if it is not
+fn check_surroundings(start: usize, end: usize, line_length: i32, content: &str) -> bool {
+    for index in start..=end {
+        let indices_to_check = vec![
+            (index as i32 - line_length - 1) as usize, // top left
+            (index as i32 - line_length) as usize,     // top
+            (index as i32 - line_length + 1) as usize, // top right
+            (index as i32 - 1) as usize,               // left
+            (index as i32 + 1) as usize,               // right
+            (index as i32 + line_length - 1) as usize, // bottom left
+            (index as i32 + line_length) as usize,     // bottom
+            (index as i32 + line_length + 1) as usize, // bottom right
+        ];
 
-        // Check for symbols in the lines above or below that are touching any of the digits
-        for &key in sorted_keys.iter() {
-            if top.len() >= key && bottom.len() >= key {
-                if (key > 0 && characters.contains(&line_chars[key - 1]))
-                    || characters.contains(&line_chars[key])
-                    || (key < line.len() - 1 && characters.contains(&line_chars[key + 1]))
-                    || characters.contains(&top.chars().nth(key).unwrap_or_default())
-                    || (key > 0 && characters.contains(&top.chars().nth(key - 1).unwrap_or_default()))
-                    || (key < top.len() - 1 && characters.contains(&top.chars().nth(key + 1).unwrap_or_default()))
-                    || characters.contains(&bottom.chars().nth(key).unwrap_or_default())
-                    || (key > 0 && characters.contains(&bottom.chars().nth(key - 1).unwrap_or_default()))
-                    || (key < bottom.len() - 1 && characters.contains(&bottom.chars().nth(key + 1).unwrap_or_default())) {
-                    valid = true;
-                    break;
+        for check_index in indices_to_check {
+            if let Some(c) = content.chars().nth(check_index) {
+                if c.is_numeric() || c == '.' {
+                    continue;
+                } else {
+                    return true;
                 }
             }
         }
-
-        if valid {
-            // Combine values based on sorted keys
-            let combined_string: String = sorted_keys
-                .iter()
-                .map(|&key| num.get(&key).unwrap())
-                .collect();
-            let as_int: i32 = combined_string.parse().unwrap();
-            *total += as_int;
-        }
-        num.clear();
     }
-}
-// wrong: 548638, 546996
 
-// Right: 550934
+    false
+}
